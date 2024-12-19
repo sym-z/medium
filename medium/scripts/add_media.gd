82798eba06d5dc8053acefbce043a34a6478ebce
Jack Sims <123803257+sym-z@users.noreply.github.com>
2024-12-18 17:37:41 -0800

    better style, no more spinboxes

--- medium/scripts/add_media.gd
+++ medium/scripts/add_media.gd
@@ -4,16 +4,22 @@ extends Control
 
 @export var titleField : LineEdit
 @export var artistField : LineEdit
-@export var yearField : SpinBox
-@export var ratingField : SpinBox
+@export var yearField : LineEdit
+@export var ratingField : LineEdit
 var musicFields : Array = []
 
 func _ready():
 	musicFields = [titleField, artistField, yearField, ratingField]
 func _on_add_button_up() -> void:
-	var numYear : int = yearField.get_line_edit().text as int
-	var numRating : int = ratingField.get_line_edit().text as int
-	Library.createAlbum(	[titleField.text,artistField.text,yearField.get_line_edit().text,ratingField.get_line_edit().text])
+	if yearField.text.is_valid_int() and ratingField.text.is_valid_int():
+		print("SICK")
+		var numYear : int = yearField.text as int
+		var numRating : int = ratingField.text as int
+	else:
+		print("Error: Non Int given for int only input.")
+		return
+
+	Library.createAlbum(	[titleField.text,artistField.text,yearField.text,ratingField.text])
 	clearFields(musicFields)
 	Library.debugAlbumLibrary()
 	
